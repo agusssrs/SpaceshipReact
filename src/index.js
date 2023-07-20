b18350487ab94e0b02b9a541c3e90a394862fd74
agusssrs <agustinmancini2@hotmail.com>
2023-07-20 01:53:54 -0300

    react

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import { PersistGate } from "redux-persist/integration/react";
 import { persistor, store } from './redux/store';
+import './components/mediasqueries.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
