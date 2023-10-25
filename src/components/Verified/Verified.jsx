a7bce1cbe3940e1d3696837712516b74a3bbcf8d
agusssrs <agustinmancini2@hotmail.com>
2023-10-24 22:22:47 -0300

    verify

--- src/components/Verified/Verified.jsx
+++ src/components/Verified/Verified.jsx
@@ -1,13 +1,15 @@
 import React, {useEffect} from 'react'
-import { Field, Form, Formik } from 'formik'
+import { Field, Form, Formik, ErrorMessage } from 'formik'
 import { Navigate,useNavigate, useParams } from 'react-router-dom';
 import axios from 'axios';
 import { BASE_URL } from '../../utils/constants';
 
 
+
 const Verified = () => {
     const navigate = useNavigate();
-    const { email } = useParams();    
+    const { email } = useParams();   
+
 
     // if (!email) {
     //     navigate('/');
@@ -18,7 +20,8 @@ const Verified = () => {
     //     return <Navigate to="/" />;
     // }
 
-    const verifyUser = async (email, code) => {
+    const verifyUser = async ({email, code}) => {
+      console.log(email,code);
         try {
           const response = await axios.patch(`${BASE_URL}auth/verify`, {email, code});     
           navigate('/');
@@ -46,27 +49,65 @@ const Verified = () => {
         <div id='logoVerified'><h2>Spaceship Agency</h2></div>
         <div className='formVerified'>
             <Formik
+                initialValues={{
+                  email:`${email}`,
+                  code: ''                  
+                }}
                 onSubmit={async (values) => {
-                  const user = await verifyUser(values.email, values.code);
+                  const user = await verifyUser(values);
                   console.log(user);
                 }}
             >
+              {({errors, touched}) => (
                 <Form>
-                    <div className="email">
-                        <label htmlFor='email'>Tu email</label>
+                  <Field type="email" name="email" id="email" value={email} readOnly>
+                    {({ field, form: { errors, touched } }) => (
+                      <div className="email">
+                        <label htmlFor='email'>Email verifición</label>
+                        <input
+                          type='email'
+                          id='email'
+                          placeholder='Email'
+                          isError={errors.email && touched.email}
+                          {...field}
+                        />
+                        <ErrorMessage name='email'>
+                          {(message) => <div>{message}</div>}
+                        </ErrorMessage>
+                      </div>
+                    )}
+                  </Field>
+                    {/* <div className="email"> */}
+                        {/* <label htmlFor='email'>Tu email</label> */}
                         {/* <input type="email" name="email" id="email" value={email} readOnly/> */}
-                        <Field type="email" name="email" id="email" value={email} readOnly/>
-                    </div>
-                    
-                    <div className="verified">
-                        <label htmlFor='verified'>Codigo de verificacion</label>
-                        {/* <Field name="code" id="code"/> */}
-                        {/* <input type="text" name='code' id='code' /> */}
-                        <Field type="text" name='verified' id='verified'/>
-                    </div>
+                        
+                        {/* <ErrorMessage name='email'></ErrorMessage> */}
+                    {/* </div> */}
+                  <Field type="text" name='code' id='code'>
+                    {({ field, form: { touched } }) => (
+                      <div className="code">
+                          <label htmlFor='code'>Codigo de verificacion</label>
+                          <input 
+                            type="text" 
+                            id='code'
+                            placeholder='code'
+                            isError={touched.code}
+                            {...field} 
+                          />
+                          
 
+                      </div>
+                    )}  
+                  </Field>  
+                    
+                  <div>
                     <button type='submit' className='verifiedBtn'>Verificate</button>
+                  </div>
+                    
                 </Form>
+              )}  
+             
+                
             </Formik>
         </div>
     </section>
